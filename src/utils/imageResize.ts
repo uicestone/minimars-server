661609f2f60fc64a87b6cf6957ae5c80e4e84f6e
Uice Lu <uicestone@gmail.com>
2020-05-09 16:44:34 +0800

    fix image resize on undefined url

--- src/utils/imageResize.ts
+++ src/utils/imageResize.ts
@@ -1,17 +1,20 @@
 export const appendResizeHtmlImage = (html: string, width?: number) =>
-  html.replace(/<img src=["'](.*?)["']>/g, (match, p1) =>
-    match.replace(p1, p1 + appendStringMaxWidth(width))
+  html.replace(
+    /<img src=["'](.*?)["']>/g,
+    (match, p1) => match && match.replace(p1, p1 + appendStringMaxWidth(width))
   );
 
 export const removeResizeHtmlImage = (html: string) =>
-  html.replace(/<img src=["'](.*?)["']>/g, (match, p1) =>
-    match.replace(p1, p1.replace(/\?.*?$/, ""))
+  html.replace(
+    /<img src=["'](.*?)["']>/g,
+    (match, p1) => match && match.replace(p1, p1.replace(/\?.*?$/, ""))
   );
 
 export const appendResizeImageUrl = (url: string, width = 750) =>
-  (url += appendStringMaxWidth(width));
+  url && url + appendStringMaxWidth(width);
 
-export const removeResizeImageUrl = (url: string) => url.replace(/\?.*?$/, "");
+export const removeResizeImageUrl = (url: string) =>
+  url && url.replace(/\?.*?$/, "");
 
 function appendStringMaxWidth(width = 1500) {
   if (process.env.ALIYUN_OSS_CROP) {
