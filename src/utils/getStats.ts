1e68816cb9d301a4cc97f92e0333349ed9894ec0
Uice Lu <uicestone@gmail.com>
2021-02-09 16:37:15 +0800

    fix flow amount by scenes

--- src/utils/getStats.ts
+++ src/utils/getStats.ts
@@ -103,16 +103,15 @@ export default async (
       return acc;
     }, {});
 
-  const flowAmountByScenes: { [gateway: string]: number } = payments.reduce(
-    (acc, payment) => {
+  const flowAmountByScenes: { [gateway: string]: number } = payments
+    .filter(p => flowGateways.includes(p.gateway))
+    .reduce((acc, payment) => {
       if (!acc[payment.scene]) {
         acc[payment.scene] = 0;
       }
       acc[payment.scene] += payment.amount;
       return acc;
-    },
-    {}
-  );
+    }, {});
 
   const couponsCount: {
     slug: string;
